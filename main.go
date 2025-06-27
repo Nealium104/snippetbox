5aed6c1ed3eb90e3ff2a08f723d5ead5ce96778c
Neal Powers <neal.grindstaff@gmail.com>
2025-06-26 22:17:28 -0400

    use Fprintf's io.writer interface to refactor to a one liner and add some headers

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// Add("key", "value")
+	w.Header().Add("server", "Go")
+
 	w.Write([]byte("Hello from Snippetbox!"))
 }
 
@@ -18,19 +21,28 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snipet with ID %d...", id)
-	w.Write([]byte(msg))
+	// msg := fmt.Sprintf("Display a specific snipet with ID %d...", id)
+	// w.Write([]byte(msg))
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a form for creating a new snippet"))
 }
 
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// You can write '201', but http comes with named status codes
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home) // Matches only "/" exactly
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home) // Matches only "/" exactly
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	log.Print("Starting server on :4000")
 
