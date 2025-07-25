5c06facb2c01fe0edfb49acbeae89cae4ab7e2b9
Neal Powers <neal.grindstaff@gmail.com>
2025-07-25 19:55:19 -0400

    add nav to template files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Directory Structure
+```bash
+.
+├── cmd
+│   └── web
+│       └── main.go
+├── go.mod
+├── internal
+└── ui
+    ├── html
+    └── static
+
+6 directories, 2 files
+```
+/cmd is for application code
+/internal is non-application-code like database models
+/ui for ui assets
---
+++ cmd/web/handlers.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "Go")
+
+	// Initialize a slice containing the paths to the two files. It's important to note that
+	// the file containing our base template must be the *first* file in the slice
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	// Use the template.ParseFiles() function to read the files and stroe the templates in a
+	// template set. Notice we use ... to pass the contents of the files slice as variadic args.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Use the ExecuteTemplate() method to write the content of the "base" as the res body
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
+	// w.Write([]byte(msg))
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet"))
+}
+
+func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	// You can write '201' instead of http.StatusCreated, but http comes with named status codes
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte("Save a new snippet..."))
+}
