d094c1d4e2ff746ba61c701c4330e3c3ba576581
Neal Powers <neal.grindstaff@gmail.com>
2025-09-27 10:27:11 -0400

    add db connection

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,31 +1,70 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	// Import the models package we just created.
+	_ "github.com/go-sql-driver/mysql"
+	"snippetbox.nealpowers/internal/models"
 )
 
+// Add a snippets field to the application struct. This will allow us to
+// use the SnippetModel type in our handlers
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// Define a new command-line flag for  the MySQL DSN string.
+	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	// To keep the main() function tidy I've put the code for creating a connection
+	// pool into the separate openDB() function below. We pass openDB() the DSN
+	// from the command-line flag
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	// We also defer a call to db.Close() so to that the connection pool is closed
+	// before the main() function exits
+	defer db.Close()
+
+	// Initalize a modelws.SnippetModel instance containing the connection pool
+	// and add it to the appliction dependencies
 	app := &application{
-		logger: logger,
+		logger:   logger,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	logger.Info("starting server", "addr", *addr)
 
-	// Call the new app.routes() method to get the servemux containing our routes,
-	// and pass taht to http.ListenAndServe().
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
